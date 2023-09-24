6524db9d578a54cd1fc348b5ecc7fdcf0f1cdafc
gorkavyi <e.gorkavyi@gmail.com>
2023-09-25 01:54:50 +0600

    Initial commit

---
+++ src/main/java/com/eg/tgcurrencybot/TgcurrencybotApplication.java
@@ -0,0 +1,13 @@
+package com.eg.tgcurrencybot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgcurrencybotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgcurrencybotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eg/tgcurrencybot/TgcurrencybotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eg.tgcurrencybot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgcurrencybotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
